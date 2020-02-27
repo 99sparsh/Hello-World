916d03a826a410b5b0887031d6e2c8070e45f395
99sparshm <99.sparsh@gmail.com>
2020-02-28 02:15:54 +0530

    UI updates

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+
+import helpers.MyEditTextDatePicker;
+
 
 public class MainActivity extends AppCompatActivity {
+
+
+    public void pickdate(View view) {
+        new MyEditTextDatePicker(this, R.id.editText5, R.style.DatePickerTheme);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
