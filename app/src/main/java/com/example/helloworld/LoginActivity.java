293f8659f3b3812209ccf5b63f1882756a15dfd0
99sparshm <99.sparsh@gmail.com>
2020-02-29 02:04:14 +0530

    Login Complete

--- app/src/main/java/com/example/helloworld/LoginActivity.java
+++ app/src/main/java/com/example/helloworld/LoginActivity.java
@@ -1,18 +1,56 @@
 package com.example.helloworld;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText emailet;
+    private EditText passet;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        emailet = findViewById(R.id.editText6);
+        passet = findViewById(R.id.editText7);
+        auth = FirebaseAuth.getInstance();
     }
     public void login(View view){
+        final String email = emailet.getText().toString();
+        final String pass = passet.getText().toString();
+        auth.signInWithEmailAndPassword(email, pass)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
 
+                            Log.d("LOGIN", "signInWithEmail:success");
+                            //FirebaseUser user = mAuth.getCurrentUser();
+                            //updateUI(user);
+                            Toast.makeText(getApplicationContext(),"Login Successful!", Toast.LENGTH_SHORT).show();
+                        } else {
+                            if(task.getException() instanceof FirebaseAuthInvalidCredentialsException)
+                                Toast.makeText(getApplicationContext(), "Invalid Credentials", Toast.LENGTH_SHORT).show();
+                            else {
+                                Log.w("LOGIN", "signInWithEmail:failure", task.getException());
+                                Toast.makeText(getApplicationContext(), "Authentication failed.", Toast.LENGTH_SHORT).show();
+                                //updateUI(null);
+                            }
+                        }
+                    }
+                });
     }
 }
