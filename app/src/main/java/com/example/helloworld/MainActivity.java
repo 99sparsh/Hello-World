0378ca994690dff28707d2d911033f1fe3310d46
unknown <deodharshardul@gmail.com>
2020-03-31 18:25:59 +0530

    Interests add,update,delete using chips, stemmer pending

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -31,10 +31,13 @@ import com.google.firebase.auth.UserProfileChangeRequest;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import helpers.MyEditTextDatePicker;
+import helpers.Post;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -179,13 +182,22 @@ public class MainActivity extends AppCompatActivity {
                                                 Log.w("user write", "Error writing document", e);
                                             }
                                         });
-
+                                add_interests_field();
                                 Toast.makeText(getApplicationContext(), "User successfully registered!", Toast.LENGTH_SHORT).show();
                             }
                         }
                     });
         }
 
+    }
+    public void add_interests_field()
+    {
+        List<String> interests = new ArrayList<>();
+        interests.add("Nothing");
+        FirebaseUser fUser = FirebaseAuth.getInstance().getCurrentUser();
+        db.collection("users") //update Firestore
+                .document(fUser.getUid())
+                .update("interests",interests);
     }
 
     public void SignIn(View view){
